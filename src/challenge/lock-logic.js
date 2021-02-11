51b53fd3c93c4ada20ea716f67879f694cd5cc08
sanjaynela <ballacksanjay@hotmail.com>
2021-02-10 23:01:09 -0800

    first commit

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -8,23 +8,47 @@ const lockState = window.mobx.observable({
 })
 
 function changeDialValue (index, incrementBy) {
-  // This part is missing some code
-  // This function is automatically called when the user clicks on a chevron
-  // it will be called with a wheel index and an amount to change the value by
-  // for example, if a user clicks on the "up" arrow for wheel 0
-  // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
-  // for example, if the user clicked the "down" arrow for the last wheel
-  // this will be called with arguments (3, -1).
+  // If incrementBy is 1
+  if (incrementBy === 1) {
+    // Add one if digit is not 9. Else, update to zero
+    let digit = window.lockState.wheels[index]
+    if (digit !== 9) {
+      digit += 1
+    } else {
+      digit = 0
+    }
+    window.lockState.wheels[index] = digit
+  } else {
+    // if incrementBy is -1
+    // Minus one if digit is not 0. Else, update to 9
+    let digit = window.lockState.wheels[index]
+    if (digit !== 0) {
+      digit -= 1
+    } else {
+      digit = 9
+    }
+    window.lockState.wheels[index] = digit
+  }
 
-  // to change the state of the lock, simply make a call like
-  // lockState.locked = false
-  // or lockState.wheels[1] = 2
-  // the lock will re-render itself when the value changes
+  // Check if the digits are the secret code
+  let i
+  let equal = 1
+  for (i = 0; i < SECRET_COMBO.length; i++) {
+    if (SECRET_COMBO[i] !== window.lockState.wheels[i]) {
+      equal = 0
+    }
+  }
+  if (equal) {
+    // If yes, updated locked to unlocked
+    window.lockState.locked = false
+  } else {
+    // If no, update locked to true
+    window.lockState.locked = true
 
-  // When the lock is set to match the secretCombo
-  // call the redirect() function with your name
-  // eg: redirect('larry-lobster')
-  // the redirect function will only redirect if the lockState is unlocked
+    // When the lock is set to match the secretCombo
+    // call the redirect() function with your name
+    redirect('sanjay-nelagadde')
+  }
 }
 
 // let our other modules find our functions
